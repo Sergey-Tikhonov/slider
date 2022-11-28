4819a1263036b9f501202e27de941247db830f0a
Sergey-Tikhonov <stikhonov163@gmail.com>
2022-11-28 18:36:28 +0400

    add buttons next and prev

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const slider = document.querySelector('#img')
 const sliderItems = Array.from(slider.children)
+const btnNext = document.querySelector('#btnNext')
+const btnPrev = document.querySelector('#btnPrev')
+
+//Клик по картинке
 
 sliderItems.forEach(function (slide, index) {
       
@@ -9,9 +13,12 @@ sliderItems.forEach(function (slide, index) {
 
     slide.dataset.index = index
 
+    sliderItems[0].setAttribute('data-active', '')
+
     slide.addEventListener('click', function () {
 
         slide.classList.add('hidden')
+        slide.removeAttribute('data-active')
 
         let nextSlideIndex
 
@@ -24,7 +31,57 @@ sliderItems.forEach(function (slide, index) {
         const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`)
         
         nextSlide.classList.remove('hidden')
+        nextSlide.setAttribute('data-active', '')
 
-        console.log(nextSlideIndex)
     })
 })
+
+//Клик по кнопке вперед
+
+btnNext.onclick = function () {
+
+    const currentSlide = slider.querySelector('[data-active]')
+    const currentSlideIndex = +currentSlide.dataset.index
+
+    currentSlide.classList.add('hidden')
+    currentSlide.removeAttribute('data-active')
+
+    let nextSlideIndex
+
+        if (currentSlideIndex + 1 === sliderItems.length) {
+            nextSlideIndex = 0
+        } else {
+            nextSlideIndex = currentSlideIndex + 1
+        }
+    
+    const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`)
+    
+    nextSlide.classList.remove('hidden')
+    nextSlide.setAttribute('data-active', '')
+
+}
+
+//Клик по кнопке назад
+
+btnPrev.onclick = function () {
+
+    const currentSlide = slider.querySelector('[data-active]')
+    const currentSlideIndex = +currentSlide.dataset.index
+
+    currentSlide.classList.add('hidden')
+    currentSlide.removeAttribute('data-active')
+
+    let nextSlideIndex
+
+        if (currentSlideIndex === 0 ) {
+            nextSlideIndex = sliderItems.length - 1
+        } else {
+            nextSlideIndex = currentSlideIndex - 1
+        }
+    
+    const nextSlide = slider.querySelector(`[data-index="${nextSlideIndex}"]`)
+    
+    nextSlide.classList.remove('hidden')
+    nextSlide.setAttribute('data-active', '')
+
+}
